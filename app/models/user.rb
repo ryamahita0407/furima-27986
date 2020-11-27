fa793642a4ec737e37ef90ea662db076830c8a87
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-11-27 17:50:52 +0900

    バリデージョン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, null: false
-  validates :first_name, null: false
-  validates :family_name, null: false
-  validates :first_name_kana, null: false
-  validates :family_name_kana, null: false
-  validates :birthday, null: false
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birthday, presence: true
 end
