69525013b7c3370dd2de3a2dccc0ee47dad98f70
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-11-27 15:55:03 +0900

    バリデージョンの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, null: false
+  validates :first_name, null: false
+  validates :family_name, null: false
+  validates :first_name_kana, null: false
+  validates :family_name_kana, null: false
+  validates :birthday, null: false
 end
