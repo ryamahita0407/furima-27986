275f8637800b3f660b3f7b85b1ad9bdc75712fd9
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-11-30 12:43:11 +0900

    with_optionsを用いてリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,37 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :first_name, presence: true,
-            format: {
-              with: /\A[ぁ-んァ-ン一-龥]/,
-              message: "は全角で入力してください"
-            }
-  validates :family_name, presence: true,
-            format: {
-              with: /\A[ぁ-んァ-ン一-龥]/,
-              message: "は全角で入力してください"
-            }
-  validates :first_name_kana, presence: true,
-            format: {
-              with: /\A[ァ-ヶー－]+\z/,
-              message: "は全角カタカナで入力してください"
-            }
-  validates :family_name_kana, presence: true,
-            format: {
-              with: /\A[ァ-ヶー－]+\z/,
-              message: "は全角カタカナで入力してください"
-            }
-  validates :birthday, presence: true
-  validates :email, presence: true,
-            format: {
-              with: /@.+/,
-              message: "は＠が含まれていません"
-            }
-  validates :password, presence:true, length: { minimum: 6 },
-            format: {
-              with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
-              message: "は半角英数字混合で入力してください"
-            }
-
+  with_options presence: true do
+    validates :nickname 
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください"} do
+      validates :first_name
+      validates :family_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください"} do
+      validates :first_name_kana
+      validates :family_name_kana
+    end
+    validates :birthday
+    validates :email,
+              format: {
+                with: /@.+/,
+                message: "は＠が含まれていません"
+              }
+    validates :password, presence:true, length: { minimum: 6 },
+              format: {
+                with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+                message: "は半角英数字混合で入力してください"
+              }
+  end
 end
