39bfdad24f2e7353046df906adf437c06786d4ce
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-01 00:34:41 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   with_options presence: true do
 
     validates :item_name, :item_text
     with_options numericality: { other_than: 1 } do
       validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id
     end 
-
-    belongs_to :user
-    belongs_to :category
-    belongs_to :condition
-    belongs_to :postage
-    belongs_to :prefecture
-    belongs_to :shipping_day
-    has_one_attached :image
   end
+    belongs_to_active_hash :category
+    belongs_to_active_hash :condition
+    belongs_to_active_hash :postage
+    belongs_to_active_hash :prefecture
+    belongs_to_active_hash :shipping_day
+    has_one_attached :image
 end
