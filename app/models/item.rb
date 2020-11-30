c70ddb5e4152e4eb2894a9ea6066b4c9169d4222
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-11-30 16:39:43 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+
+    validates :item_name, :item_text
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id
+    end 
+
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :postage
+    belongs_to :prefecture
+    belongs_to :shipping_day
+  end
 end
