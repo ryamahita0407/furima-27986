b39ce090e1e4020a18ecdbb61f12ea01f9f6bfd2
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-03 12:40:14 +0900

    editアクションのアクセス制限追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
