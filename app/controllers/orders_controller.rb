7367a2b9cd33eff0fab5f0f15820921833f1866c
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-07 16:19:22 +0900

    ordersコントローラーにアクションを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
