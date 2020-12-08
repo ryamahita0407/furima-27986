6cf20ca11753f5cd0ebdeea365cca8a361f4fc0f
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-09 01:59:28 +0900

    エラーメッセージの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
   attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_num, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :prefecture_id, numericality: { other_than: 1, message: "must be other than 1" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "は県名を選択してください" }
     validates :phone_num, format: {with: /\A\d{10}$|^\d{11}\z/, message:"は電話番号を11桁以内の番号で入力してください"}
     validates :city, :block, :token
     validates :postcode, format: {with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを含む7桁の番号を入力してください"}
