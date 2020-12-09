d99ec366d2c00d83ccc3ca3306bd659a9177765d
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-09 19:10:14 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :basic_furima
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday])
   end
+  def basic_furima
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_FURIMA_USER"] && password == ENV["BASIC_FURIMA_PASSWORD"]
+    end
+  end
+  
 end
