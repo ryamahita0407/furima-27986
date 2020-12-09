d60284811adfff41684968f45139b0f4c863a423
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-09 13:50:46 +0900

    アソシエーションの記述

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
   factory :order_address do
-    
+    association :user, :item
     postcode              {"123-4567"}
     prefecture_id         {2}
     city                  {"北海道"}
     block                 {"札幌1-1-1"}
     building              {"札幌ハイツ"}
     phone_num             {"09012345678"}
-    user_id               {2}
-    item_id               {2}
     token                 {"tok_abcdefghijk00000000000000000"}
 
   end
