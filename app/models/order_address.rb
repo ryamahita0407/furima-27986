f44dec70711b2e0973f78cd1cac40be567f57746
ryamahita0407 <73686164+ryamahita0407@users.noreply.github.com>
2020-12-09 13:50:59 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
   with_options presence: true do
     validates :prefecture_id, numericality: { other_than: 1, message: "は県名を選択してください" }
     validates :phone_num, format: {with: /\A\d{10}$|^\d{11}\z/, message:"は電話番号を11桁以内の番号で入力してください"}
-    validates :city, :block, :token
+    validates :city, :block, :token, :user_id, :item_id
     validates :postcode, format: {with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを含む7桁の番号を入力してください"}
   end
 
